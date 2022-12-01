dc1e502ae97f47dbd254ae1a2cdee2cb69560726
Ogulcan Durkan <ogdurkan@k1m17s02.42kocaeli.com.tr>
2022-12-01 19:30:12 +0300

    a

---
+++ ft_printf2/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgoc <kgoc@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/22 14:07:24 by kgoc              #+#    #+#             */
+/*   Updated: 2022/12/01 12:58:24 by kgoc             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *str, ...);
+int	ft_putchar(char c);
+int	ft_print_str(char *str);
+int	ft_int(int number);
+int	ft_u_int(unsigned int number);
+int	ft_hex_nbr(unsigned int number, char c);
+int	ft_point(unsigned long long a, int sign);
+int	ft_print_data(va_list *args, const char c);
+
+#endif
